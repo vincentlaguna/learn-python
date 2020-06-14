6c15c83016b29b22d167be5faa18168a2781adec
vincentlaguna <vincentlaguna@live.com>
2020-06-14 22:37:13 +0000

    lists in Python

--- functions_py/list_functions/list_challenges.py
+++ functions_py/list_functions/list_challenges.py
@@ -3,3 +3,10 @@ def every_three_nums(start):
   return list(range(start, 101, 3))
   
 print(every_three_nums(91))
+
+# Return a list where all elements in lst with an index between
+# start and end (inclusive) have been removed.
+def remove_middle(lst, start, end):
+  lst = lst[:start] + lst[end + 1:]
+  return lst
+print(remove_middle([4, 8, 15, 16, 23, 42], 1, 3))
