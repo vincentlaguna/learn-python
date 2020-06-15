30bba09c6f72b1200cd9d63ac5edeb9c49b4851f
vincentlaguna <vincentlaguna@live.com>
2020-06-15 21:14:29 +0000

    python list challenges continued

--- functions_py/list_functions/list_challenges.py
+++ functions_py/list_functions/list_challenges.py
@@ -10,3 +10,11 @@ def remove_middle(lst, start, end):
   lst = lst[:start] + lst[end + 1:]
   return lst
 print(remove_middle([4, 8, 15, 16, 23, 42], 1, 3))
+
+# Return either item1 or item2 depending on which item appears more often in lst
+def more_frequent_item(lst, item1, item2):
+  if lst.count(item1) >= lst.count(item2):
+    return item1
+  else:
+    return item2
+print(more_frequent_item([2, 3, 3, 2, 3, 2, 3, 2, 3], 2, 3))
