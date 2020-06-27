5b4f26a36506d0aa30a0aabe7e72b09db7f0487a
vincentlaguna <vincentlaguna@live.com>
2020-06-27 14:01:31 +0000

    update list challenges - Python

--- functions_py/list_functions/list_challenges.py
+++ functions_py/list_functions/list_challenges.py
@@ -1,4 +1,5 @@
 # Return a list of every third number between start and 100 (inclusive)
+print("Returning a list of every third number: \n")
 def every_three_nums(start):
   return list(range(start, 101, 3))
   
@@ -6,12 +7,14 @@ print(every_three_nums(91))
 
 # Return a list where all elements in lst with an index between
 # start and end (inclusive) have been removed.
+print("\nReturning a list where the middles elements have been removed: \n")
 def remove_middle(lst, start, end):
   lst = lst[:start] + lst[end + 1:]
   return lst
 print(remove_middle([4, 8, 15, 16, 23, 42], 1, 3))
 
 # Return either item1 or item2 depending on which item appears more often in lst
+print("\nReturning highest element count of two lists: \n")
 def more_frequent_item(lst, item1, item2):
   if lst.count(item1) >= lst.count(item2):
     return item1
@@ -20,7 +23,7 @@ def more_frequent_item(lst, item1, item2):
 print(more_frequent_item([2, 3, 3, 2, 3, 2, 3, 2, 3], 2, 3))
 
 # Removing elements from a list
-print("---Proceeding to eliminate even elements from this list: \n")
+print("\n---Proceeding to eliminate even elements from this list: \n")
 def delete_starting_evens(lst):
   while(len(lst) > 0 and lst[0] % 2 == 0):
     lst = lst[1:]
