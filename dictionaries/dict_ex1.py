44d5f5a228e9dcfcb37dcc8c184cfa41ea179d7a
vincentlaguna <vincentlaguna@live.com>
2020-07-06 22:13:09 +0000

    dictionary example 1 commit #2 - Python

--- dictionaries/dict_ex1.py
+++ dictionaries/dict_ex1.py
@@ -1,5 +1,11 @@
+# Examples of using dictionaries in Python
+
 user_ids = {"teraCoder": 9018293, "proProgrammer": 119238}
 
 user_ids.update({"theLooper": 138475, "stringQueen": 85739})
 
 print(user_ids)
+
+oscar_winners = {"Best Picture": "La La Land", "Best Actor": "Casey Affleck", "Best Actress": "Emma Stone", "Animated Feature": "Zootopia"}
+
+#continue to completion...
