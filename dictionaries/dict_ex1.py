66b14277294b1dd748a3fa0b35fe547575bf4408
vincentlaguna <vincentlaguna@live.com>
2020-07-07 17:16:19 +0000

    dictionary example update #2 - Python

--- dictionaries/dict_ex1.py
+++ dictionaries/dict_ex1.py
@@ -8,3 +8,12 @@ oscar_winners = {"Best Picture": "La La Land", "Best Actor": "Casey Affleck", "B
 oscar_winners["Supporting Actress"] = "Viola Davis"
 oscar_winners["Best Picture"] = "Moonlight"
 print(oscar_winners)
+
+# Using list comprehension inside a dictionary
+drinks = ["espresso", "chai", "decaf", "drip"]
+caffeine = [64, 40, 0, 120]
+zipped_drinks = zip(drinks, caffeine)
+drinks_to_caffeine = {key:value for key, value in zipped_drinks}
+print(drinks)
+print(caffeine)
+print(drinks_to_caffeine)
