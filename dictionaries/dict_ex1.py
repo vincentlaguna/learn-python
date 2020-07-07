e6d61359314a97f8a8bd851838de02593db63392
vincentlaguna <vincentlaguna@live.com>
2020-07-07 17:03:24 +0000

    dictionary example 1 update - Python

--- dictionaries/dict_ex1.py
+++ dictionaries/dict_ex1.py
@@ -1,12 +1,10 @@
 # Examples of using dictionaries in Python
-
 user_ids = {"teraCoder": 9018293, "proProgrammer": 119238}
-
 user_ids.update({"theLooper": 138475, "stringQueen": 85739})
-
 print(user_ids)
 
-oscar_winners = {"Best Picture": "La La Land", "Best Actor": "Casey Affleck", 
-                 "Best Actress": "Emma Stone", "Animated Feature": "Zootopia"}
-
-#continue to completion...
+# Changing Values in dictionaries
+oscar_winners = {"Best Picture": "La La Land", "Best Actor": "Casey Affleck", "Best Actress": "Emma Stone", "Animated Feature": "Zootopia"}
+oscar_winners["Supporting Actress"] = "Viola Davis"
+oscar_winners["Best Picture"] = "Moonlight"
+print(oscar_winners)
