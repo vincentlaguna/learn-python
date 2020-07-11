a253ca6b650ff39f33089e8c7868cf786390f714
vincentlaguna <vincentlaguna@live.com>
2020-07-11 17:44:57 +0000

    assigning and removing key values from a dictionary - Python

--- dictionaries/dict_ex1.py
+++ dictionaries/dict_ex1.py
@@ -56,3 +56,13 @@ print(tc_id)
 print("Returning superStackSmash's id (auto-assigning is non-existent) -> ")
 stack_id = user_ids.get("superStackSmash", 100000)
 print(stack_id)
+
+# Example of getting a key and removing it from the dictionary afterwards:
+print("\nExample of getting and assigning a key value and then removing it from the dictionary -> ")
+available_items = {"health potion": 10, "cake of the cure": 5, "green elixir": 20, "strength sandwich": 25, "stamina grains": 15, "power stew": 30}
+health_points = 20
+health_points += available_items.pop("stamina grains", 0)
+health_points += available_items.pop("power stew", 0)
+health_points += available_items.pop("mystic bread", 0)
+print(available_items)
+print(health_points)
