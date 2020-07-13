2446bbad760aa77fbbe1c44af1841291f385f68e
vincentlaguna <vincentlaguna@live.com>
2020-07-13 20:33:54 +0000

    scrabble challenge - commit #0 - Python

---
+++ dictionaries/scrabble_challenge.py
@@ -0,0 +1,7 @@
+# Scrabble challenge (Codecademy)
+letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+           "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 
+          4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10]
+          
+print(list(points))
