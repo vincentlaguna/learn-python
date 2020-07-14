7f8d2376bf5948abd7de4aec2967191d13da4bd9
vincentlaguna <vincentlaguna@live.com>
2020-07-14 21:23:35 +0000

    scrabble challenge update - Python

--- dictionaries/scrabble_challenge.py
+++ dictionaries/scrabble_challenge.py
@@ -5,4 +5,10 @@ points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3,
           4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10]
           
 letter_to_points = {key:value for key, value in zip(letters, points)}
+letter_to_points[" "] = 0
+
+def score_word(word):
+  point_total = 0
+  for i in word:
+    # needs implementation...
 print(letter_to_points)
