c08021e94dc285b1113a95e22f80a9b6e9551b3d
vincentlaguna <vincentlaguna@live.com>
2020-09-15 20:58:44 +0000

    using the csv library to retireive specific data from a .csv file - Python

---
+++ file_ops/read_csv1.py
@@ -0,0 +1,12 @@
+# Sample use of csv library to open a csv file as a list and output certain content
+import csv
+
+print("Retrieving data from 100-contacts.csv >>>\n")
+
+email_list = []
+
+with open('100-contacts.csv') as contacts:
+    contact_reader = csv.DictReader(contacts)
+    for row in contact_reader:
+        email_list.append(row["email"])
+print(email_list)
